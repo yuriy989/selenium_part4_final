0bf199c547e15df4422863ed77359cdc9cf12a40
yuriy989 <maloy612rus@protonmail.com>
2025-04-25 12:06:58 +0300

    Add main page test

--- conftest.py
+++ conftest.py
@@ -12,7 +12,7 @@ def pytest_addoption(parser):
     parser.addoption(
         "--language",
         action="store",
-        default="ru",
+        default="en",
         help="Choose language",
     )
     parser.addoption(
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from selenium-part4-final!")
-
-
-if __name__ == "__main__":
-    main()
