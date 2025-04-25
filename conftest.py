a0b303bcdc52f7061d4cb833e8d26ce60781b279
yuriy989 <maloy612rus@protonmail.com>
2025-04-25 09:31:22 +0300

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,58 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name",
+        action="store",
+        default="chrome",
+        help="Choose browser: chrome or firefox",
+    )
+    parser.addoption(
+        "--language",
+        action="store",
+        default="ru",
+        help="Choose language",
+    )
+    parser.addoption(
+        "--headless",
+        action="store",
+        default="false",
+        help="Headless mode: true or false",
+    )
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    headless = request.config.getoption("headless") == "true"
+
+    print(f"\nSelected browser: {browser_name}\nLanguage: {user_language}")
+
+    browser = None
+    if browser_name == "chrome":
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option(
+            "prefs", {"intl.accept_languages": user_language}
+        )
+        if headless:
+            options.add_argument("--headless=new")
+        browser = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxOptions()
+        fp.set_preference("intl.accept_languages", user_language)
+        if headless:
+            fp.add_argument("--headless")
+        browser = webdriver.Firefox(options=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+    browser.implicitly_wait(10)
+
+    yield browser
+
+    print("\nquit browser..")
+    browser.quit()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from selenium-part4-final!")
+
+
+if __name__ == "__main__":
+    main()
